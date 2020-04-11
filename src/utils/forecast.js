b8aad102750ef9aec74448ec83ccd8004af95d9a
technisRahulk <rahulkumaru99@gmail.com>
2020-04-11 20:08:46 +0530

    Added some new data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request=require('request')
+var os = require('os')
 
 const forecast=(latitude,longitude,callback)=>{
     const url='https://api.darksky.net/forecast/428359332ff7c0eff87b2f6dd18deb8a/'+ longitude + ',' + latitude + '?units=si'
@@ -10,7 +11,7 @@ const forecast=(latitude,longitude,callback)=>{
             callback('Unable to find the location',undefined)
         }
         else{
-            callback(undefined,body.daily.data[0].summary + 'It is currrently '+ body.currently.temperature + ' degrees out.There is a ' + body.currently.precipProbability +' % chance of rain.')
+            callback(undefined,body.daily.data[0].summary + 'It is currrently '+ body.currently.temperature + ' degrees out.'+os.EOL+'There is a ' + body.currently.precipProbability +' % chance of rain.'+os.EOL+'Today\'s High:'+body.daily.data[0].temperatureHigh +os.EOL+'Today\'s Low:'+body.daily.data[0].temperatureLow)
     
         }
          
